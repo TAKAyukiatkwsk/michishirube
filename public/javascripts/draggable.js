2676543a6353bb7d6f6850a49382dec391f75dba
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-05-28 23:24:40 +0900

    WIP: drag and drop tasks

--- public/javascripts/draggable.js
+++ public/javascripts/draggable.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   var $task = $(".task");
   var $task_status_block = $(".task_status_block");
+  var $dragged_task = null;
   $.event.props.push("dataTransfer");
 
   $task.on("dragstart", function(event) {
     $(this).css("opacity", "0.4");
+    $dragged_task = $(this);
     event.dataTransfer.effectAllowed = "move";
     event.dataTransfer.setData("text/html", $(this).html());
   });
@@ -16,10 +18,16 @@ $(document).ready(function() {
   });
   $task_status_block.on("dragend", function (event) {
     $(this).css("opacity", "1");
+    // ドラッグ元の要素を削除する
+    $dragged_task.remove();
   });
   $task_status_block.on("drop", function(event) {
     event.stopPropagation();
-    $(this).html(event.dataTransfer.getData("text/html"));
+    // 操作しやすいようにjQueryオブジェクトにしておく
+    var $append_task = $('<div class="task"></div>');
+    $append_task.attr({"id": $dragged_task.attr("id"), "draggable": true});
+    $append_task.html(event.dataTransfer.getData("text/html"));
+    $(this).append($append_task);
     return false;
   });
 });
